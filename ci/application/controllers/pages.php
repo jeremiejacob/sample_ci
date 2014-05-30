f1b1696cd0f9e09f0a3dbcdcf918d8547a3c44a6
jeremie jacob <j.jacob@bizwind.co.jp>
2014-05-30 13:42:50 +0800

    setting up function view of the controller

--- ci/application/controllers/pages.php
+++ ci/application/controllers/pages.php
@@ -1,16 +1,20 @@
 <?php
 
 class Pages extends CI_CONTROLLER{
-
-	public function view($page = 'home'){
-		console.log("view functon init");
-
-		if ( ! file_exists(APPATH.'/views/pages'.$page.'.php')){
+	
+	public function view($page = 'home')
+	{
+		if ( ! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
+		{
+			// Whoops, we don't have a page for that!
 			show_404();
 		}
-		$data['title'] = ucfirst($page); //Uppercase first letter
-		$this->load->view->('templates/header', $data);
+
+		$data['title'] = ucfirst($page); // Capitalize the first letter
+
+		$this->load->view('templates/header', $data);
 		$this->load->view('pages/'.$page, $data);
 		$this->load->view('templates/footer', $data);
+
 	}
 }
