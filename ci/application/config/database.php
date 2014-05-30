a02ad12b21c3e26d6eadf10004c06b802b9b5443
jeremie jacob <j.jacob@bizwind.co.jp>
2014-05-30 16:00:04 +0800

    specify db username, password, database

--- ci/application/config/database.php
+++ ci/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'vagrant';
+$db['default']['password'] = 'vagrant';
+$db['default']['database'] = 'sample';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
