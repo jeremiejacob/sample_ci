595fbd3e5226e67309e55b6ecb40a9a9b0d1215c
jeremie jacob <j.jacob@bizwind.co.jp>
2014-05-30 16:24:10 +0800

    Change from heder -> header.php

--- ci/application/controllers/news.php
+++ ci/application/controllers/news.php
@@ -13,13 +13,24 @@ class News extends CI_Controller{
 		$data['news'] = $this->news_model->get_news();
 		$data['title'] = 'News archive';
 
-		$this->load->view('templates/heder', $data);
+		$this->load->view('templates/header', $data);
 		$this->load->view('news/index', $data);
 		$this->load->view('templates/footer');
 	}
 
 	public function view($slug){
 
-		$data['news'] = $this->model->get_news($slug);
+		$data['news_item'] = $this->news_model->get_news($slug);
+		if (empty($data['news_item'])){
+			show_404();
+			log_message('info', 'error shown');
+
+		}
+
+		$data['title'] = $data['news_item']['title'];
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('news/view', $data);
+		$this->load->view('templates/footer');
 	}
 }
